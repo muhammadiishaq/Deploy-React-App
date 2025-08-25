d6501c30550f26ee3dfa039485d6c44a874bc1d9
Muhammad Ishaq <156215583+muhammadiishaq@users.noreply.github.com>
2025-08-25 23:37:37 +0500

    add a logo of GitHub in this file App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* React default logo */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WELCOME TO Waqar bhai 
-        </p>
+
+        <p>WELCOME TO Waqar bhai</p>
+
+        {/* GitHub profile link with GitHub logo */}
         <a
           className="App-link"
           href="https://github.com/muhammadiishaq"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ display: "flex", alignItems: "center", gap: "10px" }}
         >
-          Check My Github Profile Thanks.
+          {/* GitHub logo */}
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="GitHub"
+            style={{ width: "40px", height: "40px" }}
+          />
+          Check My GitHub Profile Thanks.
         </a>
       </header>
     </div>
