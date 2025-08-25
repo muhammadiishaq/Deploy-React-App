088c0451094131c8f66fe42ca77ec1c6aac4aacf
Muhammad Ishaq <156215583+muhammadiishaq@users.noreply.github.com>
2025-08-25 23:19:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO EASYDEPLOY
+          WELCOME TO Waqar bhai 
         </p>
         <a
           className="App-link"
